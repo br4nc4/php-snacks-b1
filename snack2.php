47e3d59c67684698195aafc8d8f6ca4b36f4f514
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-07-08 17:24:01 +0200

    snack2 if conditions working

--- snack2.php
+++ snack2.php
@@ -5,21 +5,21 @@ $name = key_exists("name", $_GET) ? trim($_GET["name"]) : false;
 $email = key_exists("email", $_GET) ? trim($_GET["email"]) : false;
 $age = key_exists("age", $_GET) ? trim($_GET["age"]) : false;
 
-/* if((strlen($name)) >= 3){
+if((strlen($name)) >= 3 && strpos($email, "@") !== false && strpos($email, ".") !== false && is_numeric($age)){
     echo "accesso riuscito";
 } else {
     echo "accesso negato";
 } 
 
-if(strpos($email, "@") !== false && strpos($email, ".") !== false) {
+/* if(strpos($email, "@") !== false && strpos($email, ".") !== false) {
     echo "accesso riuscito";
 } else {
     echo "accesso negato";
-} */
+}
 
 if(is_numeric($age)){
     echo "accesso riuscito";
 } else {
     echo "accesso negato";
-}
+} */
 ?>
